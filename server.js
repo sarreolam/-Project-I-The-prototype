3342cd1ce98e3f88952ab64b887ba3d1fb3945e7
Santiago Arreola Munguia <0252028@up.edu.mx>
2023-11-01 00:44:55 -0600

    para render

--- server.js
+++ server.js
@@ -64,36 +64,97 @@ const courses = {
     },
   ]
 }
+let singedIn = false 
+let error = ''
+let name = ''
+
+const users = {
+  'ADMIN': {
+    email: "yo@yo.com", 
+    pass: 'ADMIN'
+  }
+
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-
 app.use(express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
 
 
+
 app.get('/', (req, res)=> {
+
   res.redirect('/home')
 });
 
+
+
 app.get('/log-in', (req, res)=>{
-  res.render('pages/log-in')
+  res.render('pages/log-in', {error})
+  error=''
 })
+
+app.post('/log-in',(req, res)=>{
+  if (req.body.user && req.body.pass){
+    if (req.body.user in users) {
+      if (req.body.pass !== users[req.body.user].pass) {
+        console.log('buenas')
+        error = "Incorrect password!!!!"
+      }else{
+        singedIn=true
+        name=req.body.user
+        res.redirect('/courses')
+        res.end()
+        return
+      }
+    }else{
+      error = "User not found!!!!"
+    }
+  }else{
+    error = "There are missing fields to fill out"
+  }
+  res.redirect('/log-in')
+})
+
 app.get('/sing-in', (req, res)=>{
-  res.render('pages/sing-in')
+  res.render('pages/sing-in', {error})
+  error=''
 })
+
+
+app.post('/sing-in',(req, res)=>{
+  if (req.body.user && req.body.email && req.body.pass && req.body.copass){
+    if (req.body.user in users) {
+      error = "User not available!!!!"
+    }
+    else if (req.body.pass !== req.body.copass) {
+      error = "Passwords do not matchs!!!!"
+    }else{
+      users[req.body.user] = { email: req.body.email, pass: req.body.pass}
+      error= "Usuario creado exitosamente"
+      res.redirect('/log-in')
+      res.end()
+      return
+    }
+  }else{
+    error = "There are missing fields to fill out"
+  }
+  res.redirect('/sing-in')
+})
+
+
 app.get('/home',(req,res)=>{
-  res.render('pages/index')
+  res.render('pages/index',{singedIn})
 })
 
 app.get('/courses',(req,res)=>{
-  res.render('pages/courses', {courses})
+  res.render('pages/courses', {singedIn, courses, name})
 })
 
 app.get('/courses/:id',(req,res)=>{
   const id = req.params.id || null
-  res.render('pages/single-course', {id})
+  res.render('pages/single-course', {singedIn,id})
 })
 
 app.get('/courses/quiz/:id',(req,res)=>{
@@ -101,7 +162,13 @@ app.get('/courses/quiz/:id',(req,res)=>{
 })
 
 app.get('/about-us',(req,res)=>{
-  res.render('pages/about-us', {courses})
+  res.render('pages/about-us', {singedIn, courses})
+})
+
+app.post('/sing-out', (req, res)=>{
+  console.log("buenas")
+  singedIn=false
+  res.redirect('/home')
 })
 
 app.listen(port, () => {
